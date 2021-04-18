a383530127c755fce54669ee8eb594f9aa7fa5e4
Dominik Kure <dominikkure@gmail.com>
2021-04-19 00:40:30 +0200

    Writing into JSON

--- src/App.ts
+++ src/App.ts
@@ -1,6 +1,7 @@
 var fs = require('fs')
 
 const ALPHANUMERIC = new RegExp('^[a-zA-Z0-9]*$')
+const NS_PER_SEC = 1e9
 
 const validateArgv = (argv: string[]) => {
   if (argv.length > 3) throw new Error('Too many parameters given, give only one parameter!')
@@ -15,15 +16,24 @@ const processData = (str: string) => {
     .join('')
 }
 
-const constructJSON = () => {}
+const constructJSON = (input: string, output: string, duration: number) => {
+  return `{
+    "input": "${input}",
+    "output": "${output}",
+    "duration": ${duration}
+  }`
+}
 
 const main = () => {
   const time = process.hrtime()
   const argv = process.argv
   validateArgv(argv)
   const processedData = processData(argv[2])
+  const diffTuple = process.hrtime(time)
+  const diff = diffTuple[0] * NS_PER_SEC + diffTuple[1]
+  const finalJSON = constructJSON(argv[2], processedData, diff)
   try {
-    fs.writeFileSync('processed.json', processedData)
+    fs.writeFileSync('processed.json', finalJSON)
   } catch (e) {
     console.error(e)
   }
